ce5b08ab6e2a6f0277989a7b7ed297b93698b084
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2023-12-21 15:58:54 -0500

    Add BogusBuilder Solution with Contributor Entity

---
+++ Contributor.cs
@@ -0,0 +1,22 @@
+﻿using Ardalis.GuardClauses;
+using Ardalis.SharedKernel;
+
+namespace BogusBuilder;
+
+public class Contributor(
+  string email,
+  string firstName,
+  string lastName,
+  int followers,
+  int following,
+  int stars,
+  string status) : EntityBase, IAggregateRoot
+{
+  public string Email { get; internal set; } = Guard.Against.NullOrEmpty(email, nameof(email));
+  public string FirstName { get; set; } = Guard.Against.NullOrEmpty(firstName, nameof(firstName));
+  public string LastName { get; internal set; } = Guard.Against.NullOrEmpty(lastName, nameof(lastName));
+  public int Followers { get; internal set; } = Guard.Against.Negative(followers, nameof(followers));
+  public int Following { get; internal set; } = Guard.Against.Negative(following, nameof(following));
+  public int Stars { get; internal set; } = Guard.Against.Negative(stars, nameof(stars));
+  public string Status { get; internal set; } = Guard.Against.NullOrEmpty(status, nameof(status));
+}
